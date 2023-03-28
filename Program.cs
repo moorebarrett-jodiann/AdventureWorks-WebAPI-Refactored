1034e83cab071c582003d79d0bcafeaf24708c62
Paul Funston <paulfunston9@gmail.com>
2023-03-28 11:20:08 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using AdventureWorksApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AdventureWorksLt2019Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksLt2019Context"))
+);
+
+var app = builder.Build();
+
+/*var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DbContextName>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ContextClassName")));
+
+var app = builder.Build();*/
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.Run();
