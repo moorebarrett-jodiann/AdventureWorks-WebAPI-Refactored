3b77cf168eb9cafbec5cf98c5a4cf0d03d628138
Reagan615 <419247451@qq.com>
2023-03-29 14:06:44 -0500

    first

--- Models/AdventureWorksLt2019Context.cs
+++ Models/AdventureWorksLt2019Context.cs
@@ -46,7 +46,7 @@ public partial class AdventureWorksLt2019Context : DbContext
     public virtual DbSet<VProductModelCatalogDescription> VProductModelCatalogDescriptions { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Data Source=localhost\\sqlexpress;Initial Catalog=AdventureWorksLT2019;Integrated Security=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=AdventureWorksLT2019;Integrated Security=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
