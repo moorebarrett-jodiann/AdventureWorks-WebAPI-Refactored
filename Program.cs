4eac4e67118116ff1fdab92cda67c36949bd63cd
Reagan615 <419247451@qq.com>
2023-03-29 11:01:11 -0500

    c

--- Program.cs
+++ Program.cs
@@ -1,35 +1,35 @@
 using AdventureWorksApi.Models;
 using Microsoft.EntityFrameworkCore;
 
-//var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-//// Add services to the container.
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-//builder.Services.AddControllersWithViews();
-//builder.Services.AddDbContext<AdventureWorksLt2019Context>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksLt2019Context"))
-//);
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AdventureWorksLt2019Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksLt2019Context"))
+);
 
-//var app = builder.Build();
+var app = builder.Build();
 
-///*var builder = WebApplication.CreateBuilder(args);
+/*var builder = WebApplication.CreateBuilder(args);
 
-//// Add services to the container.
-//builder.Services.AddControllersWithViews();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
-//builder.Services.AddDbContext<DbContextName>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ContextClassName")));
+builder.Services.AddDbContext<DbContextName>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ContextClassName")));
 
-//var app = builder.Build();*/
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+var app = builder.Build();*/
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 
-//app.Run();
+app.Run();
